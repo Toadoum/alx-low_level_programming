caba2cff688f3efbdf084280fb2df4fd22a61669
Toadoum <toadoum.sari@aims-cameroon.org>
2022-09-08 13:43:07 -0700

    question 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print the string in the put function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
