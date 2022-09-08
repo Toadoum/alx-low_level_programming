c18d8a6cf439a4fb586560c5b8b33a8ffc72fc1d
Toadoum <toadoum.sari@aims-cameroon.org>
2022-09-08 13:45:39 -0700

    question 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - main block, this function should print the sentence
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
