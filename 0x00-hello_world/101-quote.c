8a890cf21635a7a1791f3f609a552508ad4bd50a
Toadoum <toadoum.sari@aims-cameroon.org>
2022-09-08 14:17:37 -0700

    question 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-#include <unistd.h>
+#include<unistd.h>
 /**
- * main - Our program shoul print a string followed by a new line, to the standard error.
- * Return:1
+ * main - prints the string followed by a new line to the standrad error
+ *
+ * Return: returns 1
  */
 int main(void)
 {
-char ch[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, ch, sizeof(ch));
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
